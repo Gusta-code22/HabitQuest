2f1f036a9fd6d6e485a0025bd23ff1ad45eadbab
Gusta-code22 <gustavobritomirandaxbox@gmail.com>
2026-07-05 14:49:52 -0300

    Create Users, Habits and Executions controllers
    Add three new Spring REST controllers (UserController, HabitController, ExecutionHistoryController) under br.com.Gusta_code22.habitquest.controller. Each class is annotated with @RestController and mapped to /users, /habits, and /executions respectively as empty scaffolding for future endpoint implementations.

---
+++ src/main/java/br/com/Gusta_code22/habitquest/controller/ExecutionHistoryController.java
@@ -0,0 +1,9 @@
+package br.com.Gusta_code22.habitquest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/executions")
+public class ExecutionHistoryController {
+}
